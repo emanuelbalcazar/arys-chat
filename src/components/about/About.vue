08021da943cc20ada97ec3eb475574298eb2e1e6
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2020-08-10 20:03:48 -0300

    Pagina de about con mas texto

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
     <h4>ARyS Chat</h4>
+    <br>
+    Implementación de un chat en tiempo real con autenticación de doble factor para el final de la materia de Arquitectura de Redes y Servicios.
+    <br>
+    <b>Alumnos:</b> Emanuel Balcazar y Mauricio Savarro.
+    <br>
+    El codigo fuente esta disponible en nuestro <a href="https://github.com/emanuelbalcazar/arys-chat" target="_blank">repositorio en GitHub</a>
   </div>
 </template>
 
